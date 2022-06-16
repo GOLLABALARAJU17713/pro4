7e749f027cf57311b4dc3f66d309619236869520
GOLLABALARAJU17713 <106381682+GOLLABALARAJU17713@users.noreply.github.com>
2022-06-16 16:25:34 +0530

    Add files via upload

---
+++ selinuim1.java
@@ -0,0 +1,11 @@
+package prac2;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+public class selinuim1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
